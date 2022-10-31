bdd1e5d6e064bbebf59b3cdaa0e9da5202d8a825
Medina Mauro <M.Xtreme123@Gmail.com>
2022-10-31 20:31:27 -0300

    Completado de Clases en Package Model

--- PorfolioBack/src/main/java/com/ArgProg/PorfolioBack/model/Skills.java
+++ PorfolioBack/src/main/java/com/ArgProg/PorfolioBack/model/Skills.java
@@ -1,5 +1,31 @@
 package com.ArgProg.PorfolioBack.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class Skills {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idSkill;
+    private String nombreSkill;
+    private String fotoSkill;
+    private int porcentaje;
+
+    public Skills() {
+    }
+
+    public Skills(Long idSkill, String nombreSkill, String fotoSkill, int porcentaje) {
+        this.idSkill = idSkill;
+        this.nombreSkill = nombreSkill;
+        this.fotoSkill = fotoSkill;
+        this.porcentaje = porcentaje;
+    }
+    
     
 }
