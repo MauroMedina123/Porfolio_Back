1ddf2a1ab1bae69db30f3ac75fa0c935e7702a2a
Medina Mauro <M.Xtreme123@Gmail.com>
2022-10-31 19:19:37 -0300

    Iniciando Api con dependencias y conexion a Base de Datos

---
+++ PorfolioBack/src/test/java/com/ArgProg/PorfolioBack/PorfolioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArgProg.PorfolioBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorfolioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
